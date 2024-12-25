a012d451b153edb389aa52a12566e3213554fc62
tsv2013 <tsv2013@noreply.github.com>
2024-12-25 14:07:15 +0300

    Added unit testing environment

---
+++ lib/main.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from 'vitest'
+
+describe('Utility | Main', () => {
+  test('add - should add the given two numbers', async () => {
+    expect(6).toEqual(6)
+  })
+
+  test('sub - should subtract the given two numbers', async () => {
+    expect(2).toEqual(2)
+  })
+})
